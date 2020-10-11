1741eb34e6d35dc1be93903f02395293213bf3a7
SonKn1ght <maximiilian321@gmail.com>
2020-10-11 15:34:46 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        const config = bracketsConfig.map((cur) => {
+            return cur.join('');
+        })
+        let i = 0;
+
+        while (i < config.length) {
+            while (str.includes(config[i])) {
+                str = str.replace(config[i], '');
+                i = -1;
+            }
+            i++;
+        }
+
+        return str === '';
 }
+
+
+
